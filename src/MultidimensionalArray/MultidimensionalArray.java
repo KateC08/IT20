bada5a8e9f619d0bde824c6a9341bd18cebeb88a
KateC08 <coming.katelyn08@gmail.com>
2024-09-22 15:14:01 +0800

    Added some changes on Multidimensional Array Package

--- src/MultidimensionalArray/MultidimensionalArray.java
+++ src/MultidimensionalArray/MultidimensionalArray.java
@@ -13,16 +13,18 @@ package MultidimensionalArray;
 public class MultidimensionalArray {
      public static void main(String[] args) {
          
-         //
+         //2D Array of Program and Year
          String[][] programAndyear ={
              {"BSIT", "First year"},
              {"BSBA", "Second year"},   
              {"BSED", "Third year"}
          };
+         //Process using For Loops
          for(int i = 0; i < programAndyear.length; i++) {
              for(int j = 0; j < programAndyear[i].length; j++ ) {
                  System.out.print(programAndyear[i][j] + " ");
              }
+             //Output
              System.out.println();
          }
      }
