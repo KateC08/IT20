1aa246f83eb211c7c0b75225bb13a190676b5cba
KateC08 <coming.katelyn08@gmail.com>
2024-09-22 14:51:46 +0800

    Added Simple Implementation using Array to store values

---
+++ src/SimpleImplementation/SimpleImplementation.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SimpleImplementation;
+
+/**
+ *
+ * @author HP
+ */
+public class SimpleImplementation {
+    public static void main(String[] args) {
+    
+    //Array 
+    int[] num = {1, 2, 3};    
+    System.out.println(num[0]);
+    System.out.println(num[1]);
+    System.out.println(num[2]);
+    
+    }
+    
+}
