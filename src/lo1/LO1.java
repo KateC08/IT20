1451a7f4d45991352d3b60e9700056159a0fd878
KateC08 <coming.katelyn08@gmail.com>
2024-09-22 14:23:36 +0800

    Added print statement to print all the values in every line

--- src/lo1/LO1.java
+++ src/lo1/LO1.java
@@ -19,6 +19,12 @@ public class LO1 {
         
         //Array 
         String[] names = {"Katelyn", "Katel", "keyt"};
+        
+        //Output to print the values of the Array
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println("~~~~~~~");
     }
     
 }
