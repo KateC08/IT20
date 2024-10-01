9a3739ce6cba759e1a01d6b04a189eb2de022930
KateC08 <coming.katelyn08@gmail.com>
2024-10-01 20:25:01 +0800

    Added a new method to delete an item in the list and another case condition if the delete option was chosen

--- src/topic_2_linkedlist_int/Main.java
+++ src/topic_2_linkedlist_int/Main.java
@@ -26,9 +26,10 @@ public class Main {
         while(true) {
             
              System.out.println("Kindly choose a number in the menu you want to do: "
-                + "1. Add an Item"
-                + "2. Delete an Item"
-                + "3. Move/Swap an Item");
+                + " 1. Add an Item"
+                + " 2. Delete an Item"
+                + " 3. Move/Swap an Item"
+                + " 4. Exit");
         
             int choice = sc.nextInt();
             
@@ -51,7 +52,19 @@ public class Main {
                         System.out.println("Current Linked List:");
                         list.printList();
                         break;
-            }
+                case 2:
+                        //Delete
+                        System.out.println();
+                        System.out.println("Enter a node that you want to delete: ");
+                        int item6 = sc.nextInt();
+                        
+                        System.out.println("Deleting an item from the list...");
+                        System.out.println("Current Linked List: ");
+                        
+                        list.deleteByValue(item6);
+                        list.printList();
+                        break;
+                }
         
         }
     }
