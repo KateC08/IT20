d35e0a5ed872174c1fe2721857938ffd0b452c76
KateC08 <coming.katelyn08@gmail.com>
2024-10-01 20:12:58 +0800

    Added a new method to add an item and a method to display all the item in the list on the LinkedList class and Used a switch condition to select what you want to do

--- src/topic_2_linkedlist_int/Main.java
+++ src/topic_2_linkedlist_int/Main.java
@@ -22,5 +22,37 @@ public class Main {
         
         System.out.println("Hello there! Welcome!!");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+            
+        while(true) {
+            
+             System.out.println("Kindly choose a number in the menu you want to do: "
+                + "1. Add an Item"
+                + "2. Delete an Item"
+                + "3. Move/Swap an Item");
+        
+            int choice = sc.nextInt();
+            
+            switch(choice) {
+                case 1:
+                      // Adding elements to the list
+                        System.out.println("Enter 5 Elements that you want to add: ");
+                        int item1 = sc.nextInt();
+                        int item2 = sc.nextInt();
+                        int item3 = sc.nextInt();
+                        int item4 = sc.nextInt();
+                        int item5 = sc.nextInt();
+
+                        list.add(item1);
+                        list.add(item2);
+                        list.add(item3);
+                        list.add(item4);
+                        list.add(item5);
+
+                        System.out.println("Current Linked List:");
+                        list.printList();
+                        break;
+            }
+        
+        }
     }
 }
--- src/topic_2_linkedlist_int/Node.java
+++ src/topic_2_linkedlist_int/Node.java
@@ -14,4 +14,10 @@ public class Node {
      //Initalizing variable data and Node next
      int data;  
      Node next;  
+     
+     // Constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
