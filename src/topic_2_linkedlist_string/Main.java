38a065ac7194b9e0d55efd0aaed900d8eb30b457
KateC08 <coming.katelyn08@gmail.com>
2024-10-04 18:49:26 +0800

    Added an object/instance to access the linkedlkist class, import a Scanner and a method to add a new node in the list

---
+++ src/topic_2_linkedlist_string/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package topic_2_linkedlist_string;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+		
+	//Creating an object to access the LinkedList Class
+        Linkedlist list = new Linkedlist();
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Hello there! Welcome!!");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+            
+        
+	}
+}
