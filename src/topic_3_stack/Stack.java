df885e67ee6cf4cec8e8ea999d5d2aeffafb7ca0
KateC08 <coming.katelyn08@gmail.com>
2024-10-10 16:46:34 +0800

    Added stack method to measure the size of the stack

--- src/topic_3_stack/Stack.java
+++ src/topic_3_stack/Stack.java
@@ -14,4 +14,10 @@ public class Stack {
     private int maxSize;
     private int[] stackArray;
     private int top;
+    
+    public Stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1; 
+    }
 }
