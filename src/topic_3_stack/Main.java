278c0b2c53354729093b03296b24b6ab35b2b275
KateC08 <coming.katelyn08@gmail.com>
2024-10-10 16:45:25 +0800

    Importing and creating an object of the scanner on the Main Class

--- src/topic_3_stack/Main.java
+++ src/topic_3_stack/Main.java
@@ -6,10 +6,18 @@
 
 package topic_3_stack;
 
+import java.util.Scanner;
 /**
  *
  * @author HP
  */
 public class Main {
-          
+   public static void main(String[] args) {
+       
+       Scanner sc = new Scanner(System.in);
+       
+       Stack stack = new Stack(5);
+
+       System.out.println("Hello there!");
+   }       
 }
