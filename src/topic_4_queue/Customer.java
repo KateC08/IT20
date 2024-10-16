dd5870dba705f1ae0c286edb67bf54b1d0a97670
KateC08 <coming.katelyn08@gmail.com>
2024-10-16 17:13:02 +0800

    Added a private string for the name of the customers

--- src/topic_4_queue/Customer.java
+++ src/topic_4_queue/Customer.java
@@ -11,5 +11,13 @@ package topic_4_queue;
  * @author HP
  */
 public class Customer {
-    
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
