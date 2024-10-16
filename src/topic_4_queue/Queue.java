3a9449975f432a1219c9b0d50e7daaf1cff202f0
KateC08 <coming.katelyn08@gmail.com>
2024-10-16 17:10:35 +0800

    Added a method to display the queque

--- src/topic_4_queue/Queue.java
+++ src/topic_4_queue/Queue.java
@@ -31,6 +31,17 @@ public class Queue {
         System.out.println(customer.getName() + " has been added to the queue.");
     }
     
+    public void displayQueue() {
+        if (isEmpty()) {
+            System.out.println("The queue is currently empty.");
+        } else {            System.out.print("Current queue: ");
+            for (Customer customer : customers) {
+                System.out.print(customer.getName() + " | ");
+            }
+            System.out.println();
+        }
+    }
+    
     
     
 }
