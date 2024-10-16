3dca0909d403651aae357d1e4308a762758a3173
KateC08 <coming.katelyn08@gmail.com>
2024-10-16 17:14:06 +0800

    Creating an object to access Queue Class

--- src/topic_4_queue/Main.java
+++ src/topic_4_queue/Main.java
@@ -16,7 +16,9 @@ public class Main {
 	public static void main(String[] args) {
 		
             
-             
+             Scanner sc = new Scanner(System.in);
+             Queue customerQueue = new Queue();
+		 
 	}
 
 }
