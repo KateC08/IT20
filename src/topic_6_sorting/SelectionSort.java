ff303ac4ed3bf3044d5f3ca681d0aed5d1d0197d
KateC08 <coming.katelyn08@gmail.com>
2024-11-28 13:06:00 +0800

    Added case 1 for SelectionSort

--- src/topic_6_sorting/SelectionSort.java
+++ src/topic_6_sorting/SelectionSort.java
@@ -46,6 +46,19 @@ public class SelectionSort {
                         + " 3. Exit");
                 int choice = sc.nextInt();
                 
+                 switch(choice) {
+                    case 1:
+                            System.out.println();
+                            System.out.println("Enter the number of elements you want to store in the array:");
+                            int num = sc.nextInt();
+                            arr = new int[num];
+                        
+                            System.out.println("Enter " + num + " elements.");
+                            for(int i = 0; i < num; i++) {
+                              arr[i] = sc.nextInt();
+                            }
+                            break;
+                 }
               }
               
      }
