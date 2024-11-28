aa1ea1a866fb9f6446f07f32323057c3d08f0bac
KateC08 <coming.katelyn08@gmail.com>
2024-11-28 12:27:04 +0800

    Added a default option if the users choice out of bounds

--- src/topic_6_sorting/BubbleSort.java
+++ src/topic_6_sorting/BubbleSort.java
@@ -86,7 +86,9 @@ public class BubbleSort {
                             System.out.println();
                             System.out.println("Thank you! Comeback and run it again.");
                             System.exit(0);
-                            break;    
+                            break;
+                    default:
+                            System.out.println("Invalid choice. Please select a valid option.");           
                 }
             }
         }
